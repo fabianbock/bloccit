a08cfca1d71a8f18b5f432381087ddebc3e4bb09
Fabian Bock <fabianbock@gmail.com>
2015-05-25 11:10:56 -0400

    added scopes, associations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,12 +12,12 @@ class PostsController < ApplicationController
   end
 
   def create
-     @post = Post.new(params.require(:post).permit(:title, :body))
+     @post = current_user.posts.build(params.require(:post).permit(:title, :body))
      # if @post.save
      #   flash[:notice] = "Post was saved."
      #   redirect_to @post
      # else
-       flash[:error] = "There was an error saving the post. Please try again."
+       flash.now[:error] = "There was an error saving the post. Please try again."
        render 'posts/new'
      # end
    end
